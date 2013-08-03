fa4c314e87789904aad725bf307d0bcf03a1a6fc
Matthias Urankar <matt@debianMSI>
2013-08-03 17:37:02 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
+  
+  
   has_secure_password
   validates :first_name, presence: true, length: { maximum: 25 }
   validates :last_name, presence: true, length: { maximum: 25 }
@@ -7,4 +10,20 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
       uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  
+   def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 NcoBook::Application.routes.draw do
    
    resources :users
+   resources :sessions, only: [:new, :create, :destroy]
    root  'static_pages#home'
    
+   ##########SIGN (UP/IN/OUT)
    match '/signup',  to: 'users#new',            via: 'get'
+   match '/signin',  to: 'sessions#new',         via: 'get'
+   match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
+   ######STATIC PAGES          
    match '/help',    to: 'static_pages#help',    via: 'get'
    match '/about',   to: 'static_pages#about',   via: 'get'
    match '/contact', to: 'static_pages#contact', via: 'get'
